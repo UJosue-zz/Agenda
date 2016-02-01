b6677c2be840c6b9e2728628eb4004b2521ca6b8
UJosue <estuardo1915@hotmail.com>
2016-01-31 18:22:19 -0600

    5.8
    Agregué el DDL de la base de datos.

---
+++ db/agenda.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Trial v12.18 (64 bit)
+MySQL - 5.6.26 : Database - agenda
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`agenda` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `agenda`;
+
+/*Table structure for table `contacto` */
+
+DROP TABLE IF EXISTS `contacto`;
+
+CREATE TABLE `contacto` (
+  `idContacto` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `telCasa` int(11) DEFAULT NULL,
+  `telMovil` int(11) DEFAULT NULL,
+  `correo` varchar(255) DEFAULT 'null',
+  `direccion` varchar(255) DEFAULT 'null',
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`idContacto`),
+  KEY `idUsuario` (`idUsuario`),
+  CONSTRAINT `contacto_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `nick` varchar(255) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
